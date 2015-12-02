3dbd02cf61d0910ac20d62e5e879dc19523062cc
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-12-02 04:22:30 +0200

    change profile activity to friend activity with working follow button

---
+++ app/src/main/java/com/potato/hot/hatly/FriendProfileActivity.java
@@ -0,0 +1,32 @@
+package com.potato.hot.hatly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FriendProfileActivity extends AppCompatActivity {
+
+
+    boolean following = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+
+    }
+
+    public void followClick(View view) {
+        Button follow_btn = (Button) findViewById(R.id.follow_button);
+        if (!following) {
+            follow_btn.setBackgroundColor(0xFF8c9eff);
+            follow_btn.setText("following");
+            following = true;
+        } else {
+            follow_btn.setBackgroundColor(0xFFe0e0e0);
+            follow_btn.setText("follow");
+            following = false;
+        }
+    }
+
+}
